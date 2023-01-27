10ea73197a3bdcab50fef9549b4308fef5d24e43
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-27 13:18:07 +0200

    Fix
    Add vlass

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import "../scss/app.scss";
 
+import * as R from "/node_modules/ramda";
+
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
@@ -9,4 +12,18 @@ window.addEventListener("DOMContentLoaded", () => {
     { job: "programmer", class: "is-danger" },
   ];
   const articles = document.querySelectorAll("article");
+  console.log(articles);
+  console.log(articles.length);
+  console.log(arrayToPluck[0]);
+
+
+  let x = R.pluck("class");
+  console.log(x((arrayToPluck)[0]));
+
+  for (let i = 0; i < articles.length; i++) { articles[i].classList.add(x(arrayToPluck)[i]);}
+
+
+
+
+
 });
